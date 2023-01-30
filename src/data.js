96f317349416a687b28ee6dc094f23b3bbde1e4c
aswinina <116088497+aswinina@users.noreply.github.com>
2023-01-30 16:41:38 +0100

    added more imgs

--- src/data.js
+++ src/data.js
@@ -22,4 +22,54 @@ let shopItemsData = [ {
     price:"500",
     desc:"this sofa is white color , perfact for 2 people , comfortbule to sit and relax.",
     img:"images/image4.webp"
-}]
+},{
+    id:"laptop-stand",
+    name:"Laptop Stand",
+    price:"100",
+    desc:"Roost V3 Laptop Stand – Adjustable and Extremely Portable Laptop Stand – PC and MacBook Stand",
+    img:"images/laptop-stand.jpg"
+},{
+    id:"keybord",
+    name:"Keyboard",
+    price:"120",
+    desc:"Apple Magic Keyboard - US English, Includes Lighting to USB Cable, Silver",
+    img:"images/keybord.jpg"
+},{
+    id:"lapdesk",
+    name:"Lap Desk",
+    price:"50",
+    desc:"LapGear Home Office Lap Desk with Device Ledge, Mouse Pad, and Phone Holder - Espresso Woodgrain - Fits up to 15.6 Inch Laptops - Style No. 91575",
+    img:"images/laptopstand.jpg"
+},{
+    id:"Printer",
+    name:"Printer",
+    price:"150",
+    desc:"HP OfficeJet 250 Mobile AiO 4800 x 1200DPI Thermal Inkjet A4 10ppm Wi-Fi",
+    img:"images/printer.jpg"
+},{
+    id:"chair",
+    name:"Chair",
+    price:"2000",
+    desc:"Steelcase Gesture Light-Colour with headrest.The test winner in 2017, 2018, 2019 and 2020. ",
+    img:"images/chair.jpg"
+},{
+    id:"desk",
+    name:"Desk space",
+    price:"200",
+    desc:"It is essential that you determine the height of the study desk for your needs. When using a computer, you should not rest your elbows on the desk.",
+    img:"images/desk1.webp"
+},{
+    id:"stationery",
+    name:"Stationery Holder",
+    price:"20",
+    desc:"Stationery Holder Desk Accessories Rubber Feet Box Stationery Shop Penholder Office Supplies Stationery Items Desk Organizer",
+    img:"images/Stationery-Holder.webp"
+},{
+    id:"bookself",
+    name:" Book Shelf",
+    price:"80",
+    desc:"Rotating Bookshelf 360 Degree Bookcase Home Floor Shelf Simple Multi-layer Creative Home Student Children's Picture Book Shelf",
+    img:"images/laptop-stand.jpg"
+}
+
+]
