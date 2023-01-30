dd53367500df4f3a1b04fb585511f9462f3e116b
aswinina <116088497+aswinina@users.noreply.github.com>
2023-01-30 13:10:41 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [ {
+    id:"ddfvf",
+    name:"Office Chair",
+    price:"50",
+    desc:"this chair is white color and comfortbule to sit and relax. looks very good.",
+    img:"images/image3.webp"
+},{
+    id:"gerr",
+    name:"Storage Box",
+    price:"80",
+    desc:"The office storage box is store the all office things ex pens,papers,etc....",
+    img:"images/thumbnail_405x552.webp"
+},{
+    id:"eryt",
+    name:"Table Decaration",
+    price:"30",
+    desc:"the table Decaration is decarative pice or showcase.",
+    img:"images/image2.webp"
+},{
+    id:"htrt",
+    name:"Sofa",
+    price:"500",
+    desc:"this sofa is white color , perfact for 2 people , comfortbule to sit and relax.",
+    img:"images/image4.webp"
+}]
